6e84461b3015f02368a5106a9f24c83e52580a00
aashwin-gaur <gaur.aashwin95@gmail.com>
2016-06-14 07:41:15 +1000

    committed webdriver.cs

---
+++ KutsAutomation.Framework/Driver/WebDriver.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+
+namespace KutsAutomation.Framework.Driver
+{
+    internal enum element { id, name, css, xp, link, classname };
+
+    internal class WebDriver
+    {
+        public static IWebDriver driverinstance = new PhantomJSDriver();
+
+        public static IWebDriver driver()
+        {
+            if (driverinstance.Equals(null))
+            {
+                driverinstance = new PhantomJSDriver(); 
+            }
+            
+            return driverinstance;
+        }
+    }
+}
